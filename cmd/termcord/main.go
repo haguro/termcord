d7b7eec63589579fbf032e5097affd56ea15d19b
haguro <1076+haguro@users.noreply.github.com>
2021-04-05 23:43:21 +1000

    Split termcording into two seperate packages
    This essentially separate the tool's UI from its logic.

--- cmd/termcord/main.go
+++ cmd/termcord/main.go
@@ -1,19 +1,11 @@
 package main
 
 import (
-	"log"
 	"os"
 
-	"github.com/haguro/termcord/pkg/termcorder"
+	"github.com/haguro/termcord/internal/cli"
 )
 
 func main() {
-	tc, err := termcorder.FromFlags(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if err := tc.Start(); err != nil {
-		log.Fatal(err)
-	}
+	os.Exit(cli.Run(os.Args, os.Stdin, os.Stdout, os.Stderr))
 }
